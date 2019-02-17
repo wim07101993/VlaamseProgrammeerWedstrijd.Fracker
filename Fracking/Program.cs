ebca5cd41e47de1e6b9f274de6f4c244fc104b6b
wim07101993 <van.laer.wim@gmail.com>
2019-02-17 18:37:54 +0100

    Add project files.

---
+++ Fracking/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fracking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
